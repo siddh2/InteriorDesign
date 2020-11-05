d51bc5dcead8276a9ffc3ef14da878a671ab8a90
siddh2 <siddharthg2081@gmail.com>
2020-11-05 16:24:27 +0530

    InteriorDesign

---
+++ InteriorDesign/contact/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+def contact(request):
+    if request.method == "POST":
+        message_name = request.POST['message-name']
+        message_email = request.POST['message-email']
+        message = request.POST['message']
+        
+        return render(request, 'contact.html',{'message_name':message_name})
+    else:
+        return render(request,'contact.html',{})
